33f9203c656c596ab151330f2dab37e3a72e1066
Sandip Sah <sandipsah1205@gmail.com>
2021-07-26 22:27:55 +0200

    contact form

--- js/index.js
+++ js/index.js
@@ -11,11 +11,14 @@ navLinks.forEach((link) => {
   });
 });
 
-function showConfirmation() {
-    console.log("formmm");
+function showConfirmation(form) {
+  console.log("formmm");
+  form.action = "https://formsubmit.co/sandipsah703@gmail.com";
+  form.method = "POST";
   var paragraph = document.getElementById("contactMe");
   paragraph.textContent = "Thank You, Your response has been recorded";
-  paragraph.style.textAlign = "center"
+  paragraph.style.textAlign = "center";
   var contact_title = document.getElementById("contact-me-title");
   contact_title.parentNode.removeChild(contact_title);
+
 }
