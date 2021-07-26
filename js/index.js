b1e283f8b82d10318a97a6f7c4a354fbf70b866c
Sandip Sah <sandipsah1205@gmail.com>
2021-07-26 18:19:22 +0200

    adding some feature

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav-link')
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav-link");
 
-navToggle.addEventListener('click', ()=> {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link=>{
-    link.addEventListener('click', ()=>{
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+function showAlert() {
+  var paragraph = document.getElementById("contactMe");
+  paragraph.textContent = "Thank You, Your response has been recorded";
+  paragraph.style.textAlign = "center"
+  var contact_title = document.getElementById("contact-me-title");
+  contact_title.parentNode.removeChild(contact_title);
+}
