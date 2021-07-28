16293d870b6916d5a77e2f36a0df806a85d1a6fe
Sandip Sah <sandipsah1205@gmail.com>
2021-07-28 23:33:17 +0200

    implementing contact form features

--- js/index.js
+++ js/index.js
@@ -12,26 +12,12 @@ navLinks.forEach((link) => {
 });
 
 function showConfirmation(form) {
-  // console.log("formmm");
-  // form.action = "https://formsubmit.co/sandipsah703@gmail.com";
-  // form.method = "POST";
   var paragraph = document.getElementById("contactMe");
-  paragraph.textContent = "Thank You, Your response has been recorded";
+  paragraph.textContent = "Thank You, Your response has been recorded \n I will ,prolly, text you within few hours ";
   paragraph.style.textAlign = "center";
   var contact_title = document.getElementById("contact-me-title");
   contact_title.parentNode.removeChild(contact_title);
-  //console.log("999999999999", form.name.value);
 
-  const submittedData = {
-    name: form.name.value,
-    phone: form.phone.value,
-    email: form.email.value,
-    company: form.company.value,
-    message: form.message.value,
-    _captcha: false,
-  };
-
-  console.log("the submitted form values are", submittedData);
 
   fetch("https://formsubmit.co/ajax/sandipsah703@gmail.com", {
     method: "POST",
@@ -40,36 +26,14 @@ function showConfirmation(form) {
       Accept: "application/json",
     },
     body: JSON.stringify({
-      name: "FormSubmit",
-      message: "I'm from Devro LABS",
+      name: form.name.value,
+      phone: form.phone.value,
+      email: form.email.value,
+      company: form.company.value,
+      message: form.message.value,
     }),
   })
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.log(error));
-
-  // var xhr = new XMLDocument();
-  // xhr.open(
-  //   "POST",
-  //   `https://formsubmit.co/sandipsah703@gmail.com?name=${submittedData.name}&phone=${submittedData.phone}&email=${submittedData.email}&company=${submittedData.company}&message=${submittedData.message}&_captcha=${submittedData._captcha}`,
-  //   true
-  // );
-
-  // xhr.onload = function () {
-  //   console.log("printing response------------", this.responseText);
-  // };
-  // console.log("thennnnnnnnnnnnnnnnnnnnnnnnn");
-  // The post method is yet to be implemented with xhr
-
-
-
-  
-  // postData("https://formsubmit.co/sandipsah703@gmail.com", {
-  //   "_captcha": "false",
-  //   "name": "gjdfljf",
-  //   "company": "gjlssgj",
-  //   "email": "ssah@jacobs-university.de",
-  //   "phone": "jfdlj",
-  //   "message": "gldfkjgd",
-  // })
 }
