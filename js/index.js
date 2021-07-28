bac3ebbfa104326bff375391ca500d540fe0bfcf
Sandip Sah <sandipsah1205@gmail.com>
2021-07-28 19:50:01 +0200

    adding functionality in contact form

--- js/index.js
+++ js/index.js
@@ -33,19 +33,37 @@ function showConfirmation(form) {
 
   console.log("the submitted form values are", submittedData);
 
-  var xhr = new XMLDocument();
-  xhr.open(
-    "POST",
-    `https://formsubmit.co/sandipsah703@gmail.com?name=${submittedData.name}&phone=${submittedData.phone}&email=${submittedData.email}&company=${submittedData.company}&message=${submittedData.message}&_captcha=${submittedData._captcha}`,
-    true
-  );
+  fetch("https://formsubmit.co/ajax/sandipsah703@email.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: "FormSubmit",
+      message: "I'm from Devro LABS",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 
-  xhr.onload = function () {
-    console.log("printing response------------", this.responseText);
-  };
-  console.log("thennnnnnnnnnnnnnnnnnnnnnnnn");
+  // var xhr = new XMLDocument();
+  // xhr.open(
+  //   "POST",
+  //   `https://formsubmit.co/sandipsah703@gmail.com?name=${submittedData.name}&phone=${submittedData.phone}&email=${submittedData.email}&company=${submittedData.company}&message=${submittedData.message}&_captcha=${submittedData._captcha}`,
+  //   true
+  // );
+
+  // xhr.onload = function () {
+  //   console.log("printing response------------", this.responseText);
+  // };
+  // console.log("thennnnnnnnnnnnnnnnnnnnnnnnn");
   // The post method is yet to be implemented with xhr
 
+
+
+  
   // postData("https://formsubmit.co/sandipsah703@gmail.com", {
   //   "_captcha": "false",
   //   "name": "gjdfljf",
@@ -55,5 +73,3 @@ function showConfirmation(form) {
   //   "message": "gldfkjgd",
   // })
 }
-
-
